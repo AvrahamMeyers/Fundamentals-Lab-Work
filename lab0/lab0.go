bb253bfb26d0891332b127402e929513bfdf5c3a
csfwn <csfwny@gmail.com>
2024-05-05 15:52:27 +0300

    actual lab 0 this time, most of writing implementation

--- lab0/lab0.go
+++ lab0/lab0.go
@@ -1,65 +1,157 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
 var tar0 string = "tar0"
 
-// func HandleBuy(ProductName string, Amount int, Price float32) {
+/*Should print to the end of output file the following:
 
-// 	//append to tar0.asm file:
-// 	//### BUY <ProductName> ###
-// 	//<Amount>*<Price>
-// }
+command: add
+or
+command: sub
+or
+command: neg
+*/
 
-// func HandleSell(ProductName string, Amount int, Price float32) {
+func handleAdd() string {
+	return "command: add\n"
+}
 
-// 	//append to tar0.asm file:
-// 	//$$$ CELL <ProductName> $$$
-// 	//<Amount>*<Price>
+func handleSub() string {
+	return "command: sub\n"
+}
 
-// }
+func handleNeg() string {
+	return "command: neg\n"
+}
+
+/*End Arithmetic*/
+
+/*Logic:
+Should print to the end of output file the following:
+
+command: eq
+or
+command: gt
+or
+command: lt
+
+After that it should print the current counter value and increment the counter. For example
+
+counter: 3
+*/
+
+func handleEq(cl int) string {
+	return "command: eq counter: " + strconv.Itoa(cl) + "\n"
+}
+
+func handleGt(cl int) string {
+	return "command: gt counter: " + strconv.Itoa(cl) + "\n"
+}
+
+func handleLt(cl int) string {
+	return "command: lt counter: " + strconv.Itoa(cl) + "\n"
+}
+
+/*END LOGIC*/
+/*
+Memory access:
+Should print to the end of output file the following:
+
+command: push segment: <s> index: <i>
+or
+command: pop segment: <s> index: <i>
+
+For example, for the input
+push static 2
+
+We should see in the file
+command: push segment: static index: 2
+
+*/
+func handlePush(segment string, index int) string {
+	return "command: push segment: " + segment + " index: " + strconv.Itoa(index) + "\n"
+}
+
+func handlePop(segment string, index int) string {
+	return "command: pop segment: " + segment + " index: " + strconv.Itoa(index) + "\n"
+}
+
+/*END MEMORY ACCESS*/
 
 func readfile() {
+	var counter_logical int = 0
 	//open and read the file
-	//data, err := ioutil.ReadFile("inputA.vm")
-	//if err != nil {
-	//	fmt.Print(err)
-	//}
+	data, err := os.ReadFile("inputA.vm")
+	if err != nil {
+		fmt.Print(err)
+	}
+	file, err := os.OpenFile(tar0+".asm", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
 	//fmt.Println(string(data))
-	//datastring := string(data)
+	datastring := string(data)
 
 	//split the text into lines
-	//lines := strings.Split(datastring, "\n")
+	lines := strings.Split(datastring, "\n")
 
-	//for _, line := range lines {
-	//split the line into an array(slice)
-	//words := strings.Fields(line)
-	//check if line is valid
-	// if len(words) < 3 { //for some odd reason the last line has [0/0]0x8a8b20 this as part of the line, perhaps an error somewerhe in code
-	// 	println(words)
-	// 	break
-	// }
-	//convert string to int
-	// amount, err := strconv.Atoi(words[2])
-	// if err != nil {
-	// 	fmt.Println("Error converting string to integer:", err)
-	// 	break
-	// }
-	// //convert string to float
-	// price, err := strconv.ParseFloat(words[3], 32)
-	// if err != nil {
-	// 	fmt.Println("Error converting string to integer:", err)
-	// 	break
-	// }
-	//enter appropriate function
-	// if string(words[0]) == "buy" {
-	// 	HandleBuy(string(words[1]), amount, float32(price))
-	// } else if string(words[0]) == "cell" {
-	// 	HandleSell(string(words[1]), amount, float32(price))
-	// }
-	//}
+	for _, line := range lines {
+		//split the line into an array(slice)
+		words := strings.Fields(line)
+		//todo: check if line is valid
+
+		//convert string to int
+		var str_to_add string
+		switch words[0] {
+		case "add":
+			str_to_add = handleAdd()
+		case "sub":
+			str_to_add = handleSub()
+		case "neg":
+			str_to_add = handleNeg()
+		case "eq":
+			counter_logical++
+			str_to_add = handleEq(counter_logical)
+		case "gt":
+			counter_logical++
+			str_to_add = handleGt(counter_logical)
+		case "lt":
+			counter_logical++
+			str_to_add = handleLt(counter_logical)
+		case "push":
+			index, err := strconv.Atoi(words[2])
+			if err != nil {
+				fmt.Println("Error converting string to integer:", err)
+				break
+			}
+			str_to_add = handlePush(words[1], index)
+		case "pop":
+			index, err := strconv.Atoi(words[2])
+			if err != nil {
+				fmt.Println("Error converting string to integer:", err)
+				break
+			}
+			str_to_add = handlePush(words[1], index)
+		default:
+			fmt.Println("Error: not correct line")
+		}
+		_, err = file.WriteString(str_to_add)
+		if err != nil {
+			fmt.Println("Error appending to file:", err)
+			return
+		}
+	}
 
 }
+
 func main() {
-	var counter_logical int = 0
 
 	readfile()
 }
---
+++ lab0/tar0.asm
@@ -0,0 +1,10 @@
+command: push segment: static index: 0
+command: push segment: local index: 4
+command: gt counter: 1
+command: push segment: constant index: 7
+command: gt counter: 2
+command: push segment: this index: 2
+command: push segment: constant index: 3
+command: add
+command: push segment: argument index: 1
+command: eq counter: 3
