e73bd9398dabf0e782dc25bebcd585c6438082ea
csfwn <csfwny@gmail.com>
2024-05-12 21:54:24 +0300

    pushconstant implementation revised and needs to be discussed and worked on

--- Lab_1/eq.go
+++ Lab_1/eq.go
@@ -3,5 +3,10 @@ package main
 // true if x = y and false otherwise
 
 func eq(loc_x string, loc_y string, loc_jump string) string {
-	return "@" + loc_x + "\nD = M\n@" + loc_y + "D = D - M\n"
+	return "D = D - M\n"
 }
+
+//realized that eq implements just equal this is after a push  x and push y happened
+// func eq(loc_x string, loc_y string, loc_jump string)string {
+// 	return "@" + loc_x + "\nD = M\n@" + loc_y + "D = D - M\n"
+// }
