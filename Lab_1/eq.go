ce532bd82b6c96461e1b9f54ba056be1a3f74be9
csfwn <csfwny@gmail.com>
2024-05-15 13:43:07 +0300

    fixed eq gt and ls to use the stack implementation in hack

--- Lab_1/eq.go
+++ Lab_1/eq.go
@@ -3,7 +3,14 @@ package main
 // true if x = y and false otherwise
 
 func eq() string {
-	return "D = D - M\n"
+	var str string = ""
+	str += "@sp\n"   //stack pointer
+	str += "A=M\n"   //get the value of the top of the stack
+	str += "D=M\n"   // reg(D) = the value stored at the top of the stack
+	str += "A=A-1\n" //move the stack down one or perhaps it should be the opposite up one depends on stack implementation
+	str += "D=D-M\n" //might want to do M+D if allowed because x-y x is pushed first
+	str += "M=D\n"   //load the value returned back to top of the stack
+	return str
 }
 
 //realized that eq implements just equal this is after a push  x and push y happened
--- Lab_1/ls.go
+++ Lab_1/ls.go
@@ -1,5 +1,12 @@
 package main
 
 func ls() string {
-	return "D = D - M\n"
+	var str string = ""
+	str += "@sp\n"   //stack pointer
+	str += "A=M\n"   //get the value of the top of the stack
+	str += "D=M\n"   // reg(D) = the value stored at the top of the stack
+	str += "A=A-1\n" //move the stack down one or perhaps it should be the opposite up one depends on stack implementation
+	str += "D=D-M\n" //might want to do M+D if allowed because x-y x is pushed first
+	str += "M=D\n"   //load the value returned back to top of the stack
+	return str
 }
