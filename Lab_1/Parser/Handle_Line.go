a4332cba0ce9267cfa9493f303d36281e22efba2
AvrahamMeyers <a32100meyers@gmail.com>
2024-05-15 21:35:13 +0300

    working on handle line func

--- Lab_1/Parser/Handle_Line.go
+++ Lab_1/Parser/Handle_Line.go
@@ -1,45 +1,49 @@
 package Parser
 
+import (
+	"strings"
+)
+
 func Handle_line(line string, counter_pointer *int) string {
-	//words := strings.Fields(line)
+	words := strings.Fields(line)
 	//todo: check if line is valid
 
 	counter_logical := *counter_pointer
 	//convert string to int
 	var str_to_add string
-	// switch words[0] {
-	// case "add":
-	// 	str_to_add = handleAdd()
-	// case "sub":
-	// 	str_to_add = handleSub()
-	// case "neg":
-	// 	str_to_add = handleNeg()
-	// case "eq":
-	// 	counter_logical++
-	// 	str_to_add = handleEq(counter_logical)
-	// case "gt":
-	// 	counter_logical++
-	// 	str_to_add = handleGt(counter_logical)
-	// case "lt":
-	// 	counter_logical++
-	// 	str_to_add = handleLt(counter_logical)
-	// case "push":
-	// 	index, err := strconv.Atoi(words[2])
-	// 	if err != nil {
-	// 		fmt.Println("Error converting string to integer:", err)
-	// 		break
-	// 	}
-	// 	str_to_add = handlePush(words[1], index)
-	// case "pop":
-	// 	index, err := strconv.Atoi(words[2])
-	// 	if err != nil {
-	// 		fmt.Println("Error converting string to integer:", err)
-	// 		break
-	// 	}
-	// 	str_to_add = handlePop(words[1], index)
-	// default:
-	// 	fmt.Println("Error: not correct line")
-	// }
+	switch words[0] {
+	case "add":
+		str_to_add = CodeWriter.add()
+		// case "sub":
+		// 	str_to_add = handleSub()
+		// case "neg":
+		// 	str_to_add = handleNeg()
+		// case "eq":
+		// 	counter_logical++
+		// 	str_to_add = handleEq(counter_logical)
+		// case "gt":
+		// 	counter_logical++
+		// 	str_to_add = handleGt(counter_logical)
+		// case "lt":
+		// 	counter_logical++
+		// 	str_to_add = handleLt(counter_logical)
+		// case "push":
+		// 	index, err := strconv.Atoi(words[2])
+		// 	if err != nil {
+		// 		fmt.Println("Error converting string to integer:", err)
+		// 		break
+		// 	}
+		// 	str_to_add = handlePush(words[1], index)
+		// case "pop":
+		// 	index, err := strconv.Atoi(words[2])
+		// 	if err != nil {
+		// 		fmt.Println("Error converting string to integer:", err)
+		// 		break
+		// 	}
+		// 	str_to_add = handlePop(words[1], index)
+		// default:
+		// 	fmt.Println("Error: not correct line")
+	}
 	*counter_pointer = counter_logical
 	return str_to_add
 }
