8dd3976c8316c9cd47ce25941a05d589048bd5e5
Avi-W <avi.weiner12@gmail.com>
2024-05-15 21:09:16 +0300

    finished addSubNeg

--- Lab_1/CodeWriter/addSubNeg.go
+++ Lab_1/CodeWriter/addSubNeg.go
@@ -11,13 +11,17 @@ func add() string {
 }
 
 func sub() string {
-	return "@sp\n" +
-		"D = A\n" +
-		"@y \n" +
-		"D = D - A\n"
+	return "@sp\n" + // A = 0, the location of SP
+		"M=M-1\n" + // SP-- (Move stack pointer one down)
+		"A=M\n" + // A = SP (Save this pointer in A)
+		"D=M\n" + // D = M[A] (the value at the top of the stack)
+		"@sp\n" + // A = 0, the location of SP
+		"A=M-1\n" + // A = M[A] - 1 (the location of the second of the stack)
+		"M=M-D\n" // M[A] = D + M[A] (the value at the top of the stack)
 }
 
 func neg() string {
-	return "@y \n" +
-		"D = -A\n"
+	return "@sp\n" +
+		"A=M-1\n" +
+		"M=-M\n"
 }
