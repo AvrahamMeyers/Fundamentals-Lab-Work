21a0a73ddc881bc61dca81e09afbe8250ba0c0ed
csfwn <csfwny@gmail.com>
2024-05-15 18:24:23 +0300

    pushConstant.go changed to fit format as other readable files

--- Lab_1/pushConstant.go
+++ Lab_1/pushConstant.go
@@ -2,7 +2,11 @@ package main
 
 //expected is just the number string return the commands @num  D=A
 func pushConstant(num string) string {
-	return "@" +
-		num +
-		"\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
+	return "@" + num + "\n" + //set A = to number
+		"D=A\n" + //set D = to num
+		"@SP\n" + //set A = SP (0)
+		"A=M\n" + //set A = RAM[SP]
+		"M=D\n" + //set RAM[Ram[SP]] = to num
+		"@SP\n" + //A = SP
+		"M=M+1\n" //set SP to be +1
 }
