428ad62236c1bbd0731e7694c549b0d4c9be9f8b
Avi-W <avi.weiner12@gmail.com>
2024-05-15 15:46:31 +0300

    finished add

--- Lab_1/addSubNeg.go
+++ Lab_1/addSubNeg.go
@@ -1,28 +1,14 @@
 package main
 
-//func add() string {
-//	return "@sp\n" +
-//		"D = M\n" +
-//		"A = A - 1\n" +
-//		"MD = D + M\n"
-//}
-
 func add() string {
 	return "@sp\n" + // A = 0, the location of SP
 		"M=M-1\n" + // SP-- (Move stack pointer one down)
 		"A=M\n" + // A = SP (Save this pointer in A)
-		"D=M\n" + // D = M[A] (the value at the second top of the stack)
+		"D=M\n" + // D = M[A] (the value at the top of the stack)
 		"@sp\n" + // A = 0, the location of SP
-		"A=M-1\n" + // A = M[A] - 1 (the location of the top of the stack)
+		"A=M-1\n" + // A = M[A] - 1 (the location of the second of the stack)
 		"M=D+M\n" // M[A] = D + M[A] (the value at the top of the stack)
 }
-
-//func sub() string {
-//	return "@x \n" +
-//		"D = A\n" +
-//		"@y \n" +
-//		"D = D - A\n"
-//}
 
 func sub() string {
 	return "@sp\n" +
