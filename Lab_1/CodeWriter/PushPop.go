d7af012abeda9fdf8e1668c1cf0d121d1d9a62bf
AvrahamMeyers <a32100meyers@gmail.com>
2024-05-16 20:47:41 +0300

    generalized push and pop for group 1 memory segments

--- Lab_1/CodeWriter/PushPop.go
+++ Lab_1/CodeWriter/PushPop.go
@@ -4,22 +4,24 @@ import (
 	"strconv"
 )
 
-// PopLocal pushes the value at the top of the stack to the local segment offset by index
-func PopLocal(index int) string {
+// Group 1 Memory Segments: local, argument, this, that
+
+// PopGroup1 pushes the value at the top of the stack to the specified segment offset by index
+func PopGroup1(index int, segment string) string {
 	return "@SP\n" + // A = 0, the location of SP
 		"A=M\n" + // A = SP (Save this pointer in A)
 		"D=M\n" + // D = M[A] (the value at the top of the stack)
 		"@SP\n" + // A = 0, the location of SP
 		"M=M-1\n" + // SP-- (Move stack pointer one down)
-		"@LCL\n" + // A = 1, the location of LCL
-		"A=A+" + strconv.Itoa(index) + "\n" + // A = LCL + index
+		"@" + segment + "\n" + // A = 1, the location of the segment
+		"A=A+" + strconv.Itoa(index) + "\n" + // A = segment + index
 		"M=D\n" // M[A] = D (the value at the top of the stack)
 }
 
-// PushLocal pushes the value at the local segment offset by index to the top of the stack
-func PushLocal(index int) string {
-	return "@LCL\n" + // A = 1, the location of LCL
-		"A=A+" + strconv.Itoa(index) + "\n" + // A = LCL + index
+// PushGroup1 pushes the value at the specified segment offset by index to the top of the stack
+func PushGroup1(index int, segment string) string {
+	return "@" + segment + "\n" + // A = 1, the location of the segment
+		"A=A+" + strconv.Itoa(index) + "\n" + // A = segment + index
 		"D=M\n" + // D = M[A] (the value at the local segment)
 		"@SP\n" + // A = 0, the top of the stack
 		"A=M\n" + // A = SP (Save this pointer in A)
