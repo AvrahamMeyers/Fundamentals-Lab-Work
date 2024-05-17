14bd6e6c6e4c58244d66a3709291a688b51c30f7
Avi-W <avi.weiner12@gmail.com>
2024-05-17 17:07:15 +0300

    new Eq() attempt

--- Lab_1/CodeWriter/eqGtLt.go
+++ Lab_1/CodeWriter/eqGtLt.go
@@ -11,6 +11,29 @@ func Eq() string {
 		"M=D\n" //load the value returned back to top of the stack
 }
 
+// Attempt at Eq by Avi
+func Eq() string {
+	return "@SP\n" + // A = 0, the location of SP
+		"M=M-1\n" + // SP-- (Move stack pointer one down)
+		"A=M\n" + // A = SP (Save this pointer in A)
+		"D=M\n" + // D = M[A] (the value at the top of the stack)
+		"@SP\n" + // A = 0, the location of SP
+		"A=M-1\n" + // A = M[A] - 1 (the location of the top of the stack)
+		"D=M-D\n" + // D = M[A] - D (the value at the top of the stack)
+		"@Equal\n" + // jump to Equal if x = y (x-y = 0)
+		"D;JEQ\n" + // if x != y, jump to Equal
+		"@SP\n" + // A = 0, the location of SP
+		"A=M-1\n" + // A = M[A] - 1 (the location of the top of the stack)
+		"M=-1\n" + // M[A] = -1 top of the stack is -1 as not equal
+		"@End\n" + // jump to End
+		"0;JMP\n" + // jump to End
+		"(Equal)\n" + // label Equal
+		"@SP\n" + // A = 0, the location of SP
+		"A=M-1\n" + // A = M[A] - 1 (the location of the top of the stack)
+		"M=0\n" + // M[A] = 0 top of the stack is 0 as equal
+		"(End)\n" // label End
+}
+
 func Gt() string {
 	return "@SP\n" + //stack pointer.
 		"A=M\n" + //get the value of the top of the stack.
