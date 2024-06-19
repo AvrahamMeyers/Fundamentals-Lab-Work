db0a1c4bec2f2185e3037b821ba12f84a2637147
csfwn <csfwny@gmail.com>
2024-06-19 14:51:13 +0300

    added comiler class

---
+++ Lab_4/CompilationEngine/Compiler.go
@@ -0,0 +1,85 @@
+package compilationengine
+
+type comp struct {
+}
+
+func (X *comp) Constructor() {
+	/*Creates a new compilation
+	engine with the given input and
+	output. The next routine called
+	must be compileClass()..*/
+
+}
+
+func (X *comp) CompileClass() {
+	//Compiles a complete class.
+}
+
+func (X *comp) CompileClassVarDec() {
+	/*Compiles a static declaration or
+	a field declaration.*/
+}
+
+func (X *comp) CompileSubroutine() {
+	//Compiles a complete method,
+	//function, or constructor.
+}
+
+func (X *comp) CompileParameterList() {
+	/*Compiles a (possibly empty)
+	parameter list, not including the
+	enclosing ‘‘()’’..*/
+}
+
+func (X *comp) CompileVarDec() {
+	/* Compiles a var declaration..*/
+}
+
+func (X *comp) CompileStatements() {
+	/* Compiles a sequence of state
+	ments, not including the
+	 enclosing ‘‘{}’’..*/
+}
+
+func (X *comp) CompileDo() {
+	/* Compiles a do statement..*/
+}
+
+func (X *comp) CompileLet() {
+	/*  Compiles a let statement..*/
+}
+func (X *comp) CompileWhile() {
+	/* Compiles a while statement..*/
+}
+func (X *comp) CompileReturn() {
+	/*  Compiles a return statement.*/
+}
+func (X *comp) CompileIf() {
+	/*  Compiles an if statement, pos
+	sibly with a trailing else clause.T.*/
+}
+func (X *comp) CompileExpression() {
+	/* Compiles an expression..*/
+}
+func (X *comp) CompileTerm() {
+	/*Compiles a term. This routine is
+	 faced with a slight difficulty
+	 when trying to decide between
+	 some of the alternative parsing
+	 rules. Specifically, if the current
+	 token is an identifier, the routine
+	 must distinguish between a
+	 variable, an array entry, and a
+	 subroutine call. A single look
+	ahead token, which may be one
+	 of ‘‘[’’, ‘‘(’’, or ‘‘.’’ suffices to dis
+	tinguish between the three possi
+	bilities. Any other token is not
+	 part of this term and should not
+	 be advanced over.*/
+}
+func (X *comp) CompileExpressionList() {
+	/*Compiles a (possibly empty)
+	comma-separated list of
+	expressions.*/
+}
--- Lab_4/JackTokenizer/Tokenizer.go
+++ Lab_4/JackTokenizer/Tokenizer.go
@@ -5,18 +5,18 @@ type Tokenizer struct {
 	//here we may want items such as current token file pointer
 }
 
-func (X Tokenizer) Constructor() {
+func (X *Tokenizer) Constructor() {
 	/*Opens the input file/stream and gets
 	ready to tokenize it.*/
 
 }
 
-func (X Tokenizer) HasMoreTokens() bool {
+func (X *Tokenizer) HasMoreTokens() bool {
 	//Do we have more tokens in the input?
 	return false
 }
 
-func (X Tokenizer) Advance() {
+func (X *Tokenizer) Advance() {
 	/*Gets the next token from the input
 	and makes it the current token. This
 	method should only be called if
@@ -24,35 +24,35 @@ func (X Tokenizer) Advance() {
 	there is no current token.*/
 }
 
-func (X Tokenizer) TokenType() {
+func (X *Tokenizer) TokenType() {
 	//Returns the type of the current token
 }
 
-func (X Tokenizer) keyWord() {
+func (X *Tokenizer) keyWord() {
 	/*Returns the keyword which is the
 	current token. Should be called only
 	when tokenType() is KEYWORD.*/
 }
 
-func (X Tokenizer) Symbol() {
+func (X *Tokenizer) Symbol() {
 	/*Returns the character which is the
 	current token. Should be called only
 	when tokenType() is SYMBOL.*/
 }
 
-func (X Tokenizer) Identifier() {
+func (X *Tokenizer) Identifier() {
 	/*Returns the identifier which is the
 	current token. Should be called only
 	when tokenType() is IDENTIFIER.*/
 }
 
-func (X Tokenizer) IntVal() {
+func (X *Tokenizer) IntVal() {
 	/*Returns the integer value of the
 	current token. Should be called only
 	when tokenType() is INT_CONST.*/
 }
 
-func (X Tokenizer) StringVal() {
+func (X *Tokenizer) StringVal() {
 	/* Returns the string value of the current
 	token, without the double quotes.
 	Should be called only when
